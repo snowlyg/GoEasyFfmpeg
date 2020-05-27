af2b6f681c1170a062f45ab8b33d31349bd94833
snowlyg <569616226@qq.com>
2020-05-27 09:48:28 +0800

    增加后台 cmd 管理

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 CWD=$(cd "$(dirname $0)";pwd)
 "$CWD"/easydarwin install
 "$CWD"/easydarwin start
-#"$CWD"/rtsp-server/rtsp-server install
-#"$CWD"/rtsp-server/rtsp-server start
+"$CWD"/rtsp-server/rtsp-server install
+"$CWD"/rtsp-server/rtsp-server start
