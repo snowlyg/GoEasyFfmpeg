ea8d2f49edd724d2f1c9c764cd0a4761cf570a97
snowlyg <569616226@qq.com>
2020-05-27 09:52:30 +0800

    增加后台 cmd 管理

--- stop.sh
+++ stop.sh
@@ -2,5 +2,5 @@
 CWD=$(cd "$(dirname $0)";pwd)
 "$CWD"/easydarwin stop
 "$CWD"/easydarwin uninstall
-"$CWD"/rtsp-server/rtsp-server stop
-"$CWD"/rtsp-server/rtsp-server uninstall
+"$CWD"rtsp-server stop
+"$CWD"rtsp-server uninstall
