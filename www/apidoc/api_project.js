c1eb4a083332532741ecbd219a47ab9c85fec9de
snowlyg <569616226@qq.com>
2021-03-31 13:36:37 +0800

    www

---
+++ www/apidoc/api_project.js
@@ -0,0 +1,32 @@
+define({
+  "name": "GoEasyFfmpeg",
+  "title": "GoEasyFfmpeg API Reference",
+  "order": [
+    "stats",
+    "Pushers",
+    "ers",
+    "stream",
+    "StreamStart",
+    "StreamStop",
+    "record",
+    "RecordFolders",
+    "RecordFiles",
+    "sys",
+    "Login",
+    "Logout",
+    "GetUserInfo",
+    "ModifyPassword",
+    "GetServerInfo"
+  ],
+  "version": "9.0.0",
+  "description": "EasyDarwin Open Source Media Server",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-03-31T05:34:27.640Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
